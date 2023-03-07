3e931bae6282da54f1de5e770cd402be0f30bbcc
Helen Flynn <flynnhelen17@gmail.com>
2023-03-06 19:17:04 -0700

    test2

---
+++ setup.R
@@ -0,0 +1,22 @@
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgdal', 
+              'rgbif',
+              'soilDB')
+
+packageLoad(packages)
